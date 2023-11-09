3ce61dd738aa789e97d1fde7590bcc61bada9cae
MohamedHegazy2020 <48334725+MohamedHegazy2020@users.noreply.github.com>
2023-11-09 12:25:53 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,95 +1,89 @@
 let city = document.querySelector('[placeholder="Find your location..."]');
 let find = document.querySelector('[value="Find"]');
 let noOfDays = document.querySelectorAll(".dropdown-item");
-let  days = 3 ; 
-let  searchrdCity = "cairo" ; 
+let days =3;
+let searchrdCity = "cairo";
 
-
-
-
-
-getWeather(searchrdCity , days)
+getWeather(searchrdCity, days);
 const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
 ];
 
 const d = new Date();
+// console.log(d.getDay());
 
-getDays ()
-
+getDays();
 
 function getDays() {
+  let d
+	for (const day of noOfDays) {
+		day.addEventListener("click", function () {
+			d = Number(this.value);
+      getWeather(searchrdCity , d)
+			// console.log(days);
+		});
+	}
+  // return d;
+  days =d
 
-for (const day of noOfDays) {
-  day.addEventListener('click' , function(){
-
-    days = Number(this.value)
-    console.log(days);
-    return days;
-  })
-
-  
-}
-
-  
 }
 
 find.addEventListener("click", function () {
-
-  searchrdCity = city.value;
-  search(searchrdCity);
+	searchrdCity = city.value;
+	search(searchrdCity);
 });
 city.addEventListener("keyup", function (e) {
-  if (e.code == "Enter") {
-   searchrdCity = city.value;
-   search(searchrdCity);
-  }
+	if (e.code == "Enter") {
+		searchrdCity = city.value;
+		search(searchrdCity);
+	}
 });
 
 function search(x) {
-  if (x != "") {
-    getWeather(searchrdCity , days);
-  } else {
-    alert("enter a location ,please...");
-  }
+	if (x != "") {
+		getWeather(searchrdCity, days);
+	} else {
+		alert("enter a location ,please...");
+	}
 }
 
-async function getWeather(c , d) {
-  let weatherReq = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=3c3be09e214847aeb22182734232702&q=${c}&aqi=yes&days=${d}`
-  );
+async function getWeather(c, d) {
+	let weatherReq = await fetch(
+		`http://api.weatherapi.com/v1/forecast.json?key=3c3be09e214847aeb22182734232702&q=${c}&aqi=yes&days=${d}`
+	);
 
-  let res = await weatherReq.json();
+	let res = await weatherReq.json();
+	console.log(res);
 
-  displayWeather(res);
+	displayWeather(res);
 }
 
 function displayWeather(r) {
-  let headerCell = "";
-  let bodyCell = "";
+	let headerCell = "";
+	let bodyCell = "";
 
-  for (const day of r.forecast.forecastday) {
-    headerCell += `<th>
+	for (const day of r.forecast.forecastday) {
+		headerCell += `<th>
   <div class="d-flex justify-content-between">
-    <span>${weekday[d.getDay(day.date)]}</span>
+    <span>${weekday[d.getDay() + r.forecast.forecastday.indexOf(day)]}</span>
     <span>${day.date}</span>
   </div>
 </th>`;
 
-    bodyCell += `<td class="p-4 mx-4">
+		bodyCell += `<td class="p-4 mx-4">
                   <div>
                     <div class="location">${r.location.name}</div>
 
                     <div class="degree d-flex align-items-center justify-content-around">
                       <div class="num text-white">${
-                        day.day.avgtemp_c
-                      }<sup>o</sup>C</div>
+												day.day.avgtemp_c
+											}<sup>o</sup>C</div>
 
                       <div class="forecast-icon my-5">
                         <img
@@ -116,9 +110,9 @@ function displayWeather(r) {
                     </div>
                   </div>
                 </td>`;
-  }
+	}
 
-  //    console.log(headerCell);
-  document.querySelector(".forecast-table thead tr ").innerHTML = headerCell;
-  document.querySelector(".forecast-table tbody tr ").innerHTML = bodyCell;
+	//    console.log(headerCell);
+	document.querySelector(".forecast-table thead tr ").innerHTML = headerCell;
+	document.querySelector(".forecast-table tbody tr ").innerHTML = bodyCell;
 }
